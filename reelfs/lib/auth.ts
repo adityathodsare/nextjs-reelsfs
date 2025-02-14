31a84c29440262038f5de8a2b2b8701fb04a3df4
adityathodsare <thodsareaditya@gmail.com>
2025-02-14 15:57:47 +0530

    Your message

---
+++ reelfs/lib/auth.ts
@@ -0,0 +1,75 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        phoneNumber: { label: "phoneNumber", type: "text " },
+        password: { label: "Password ", type: "password " },
+      },
+
+      async authorize(credentials) {
+        if (credentials?.email || credentials?.password) {
+          throw new Error("email or password is missing ");
+        }
+
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials?.email });
+
+          if (!user) {
+            throw new Error(" user NOT found ");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials!.password,
+            user.password
+          );
+          if (!isPasswordCorrect) {
+            throw new Error(" password is NOT correct ");
+          }
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          throw error;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    //
+
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+
+    //
+
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+
+    //
+  },
+
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+
+  secret: process.env.NEXTAUTH_SECRET,
+};
